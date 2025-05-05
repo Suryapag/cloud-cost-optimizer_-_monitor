a86151bcce6b85cf70b33455a37ed0e439f73884
Suryapag <suryapa9092@gmail.com>
2025-05-05 17:46:10 +0530

    first commit

---
+++ src/aws_ec2_check.py
@@ -0,0 +1,18 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def list_instances():
+    ec2 = boto3.client('ec2', 
+                       aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+                       aws_secret_access_key=os.getenv('AWS_SECRET_kEY'))
+    
+    instances = ec2.describe_instances()
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            print(f"Instance ID: {instance['InstanceId']}, State: {instance['State']['Name']}")
+
+if __name__ == "__main__":
+    list_instances()
